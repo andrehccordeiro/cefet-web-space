5faf93eddd606b24a04c43572d0be7d6914f1145
andrehccordeiro <andrecordeiro0931@gmail.com>
2026-04-01 10:23:22 -0300

    Exercício 01
    Exercício 01 para calcular a Gravitação Universal.

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+const constante = 6.67e-11
+let massa1 = document.querySelector("#massa1");
+let massa2 = document.querySelector("#massa2");
+let distancia = document.querySelector("#distancia");
+let resultado = document.querySelector("#resultado");
+let botao = document.querySelector("#calcular")
+
+
+botao.addEventListener('click', () => {
+    let m1 = parseFloat(massa1.value);
+    let m2 = parseFloat(massa2.value);
+    let d = parseFloat(distancia.value);
+    let total = (constante*m1*m2)/(d**2)
+    resultado.value = total;
+})
