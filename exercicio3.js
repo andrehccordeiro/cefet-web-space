d073c3b4d7f2e2c3a1526a57189204266ad54ccb
andrehccordeiro <andrecordeiro0931@gmail.com>
2026-04-01 19:39:09 -0300

    Exercício 03
    Realização do exercício 03. Agora ao clicar no botão expande ou retrai o texto.

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,19 @@
 // Faça o exercício dos PARÁGRAFOS aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+let listaBotoesEl = document.querySelectorAll('.botao-expandir-retrair')
+
+
+for(let botaoEl of listaBotoesEl){
+    botaoEl.addEventListener('click', (e) => {
+        let botaoEl = e.currentTarget;
+        let paragrafoEl = botaoEl.parentNode;
+        paragrafoEl.classList.toggle('expandido');   
+        
+        if(paragrafoEl.classList.contains('expandido')){
+            botaoEl.innerHTML = '-';
+        }else{
+            botaoEl.innerHTML = '+';
+        }
+    
+    })
+
+}
